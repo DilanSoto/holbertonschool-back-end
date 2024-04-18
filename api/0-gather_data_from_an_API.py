54990dbfd1de157c14d0eb0f3607e5093c3ff328
DilanSoto <dilanso12@gmail.com>
2024-04-18 13:23:55 -0400

    pycode fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,55 +1,23 @@
 #!/usr/bin/python3
+
 """
-This script retrieves and displays the progress of a specific employee's TODO list.
-It uses the JSONPlaceholder API to get the data.
+Write a Python script that, using this REST API, for a given employee ID
+returns information about his/her TODO list progress.
 """
 
 import requests
-import sys
-
-def get_employee_todo_progress(employee_id):
-    """
-    Retrieve and display the employee's TODO list progress
-    """
-    todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-        employee_id)
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-        employee_id)
-
-    try:
-        todos_response = requests.get(todos_url)
-        user_response = requests.get(user_url)
-        todos_response.raise_for_status()
-        user_response.raise_for_status()
-
-        todos = todos_response.json()
-        user = user_response.json()
-
-        employee_name = user['name']
-
-        completed_tasks = [todo for todo in todos if todo['completed']]
-        num_completed_tasks = len(completed_tasks)
-        total_tasks = len(todos)
-
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name, num_completed_tasks, total_tasks))
-        for todo in completed_tasks:
-            print("\t {}".format(todo['title']))
-
-    except requests.exceptions.HTTPError as errh:
-        print("HTTP Error:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:", errc)
-    except requests.exceptions.Timeout as errt:
-        print("Timeout Error:", errt)
-    except requests.exceptions.RequestException as err:
-        print("Something Else:", err)
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('Usage: {} <employee_id>'.format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    user = response.json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv
+                                                                        [1])
+    response = requests.get(url)
+    todos = response.json()
+    completed = [todo.get("title") for todo in todos if todo.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed),
+                                                          len(todos)))
+    [print("\t {}".format(todo)) for todo in completed]
