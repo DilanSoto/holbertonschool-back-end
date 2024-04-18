2cc0af36f1bf7dde5342b0cf810782fc1c2b4a38
DilanSoto <dilanso12@gmail.com>
2024-04-18 13:13:04 -0400

    pycode fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,50 +1,50 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Script to retrieve and display information about an employee's TODO list progress
+This script retrieves and displays the progress of a specific employee's TODO list.
+It uses the JSONPlaceholder API to get the data.
 """
-import sys
+
 import requests
+import sys
 
 def get_employee_todo_progress(employee_id):
     """
     Retrieve and display the employee's TODO list progress
     """
-    # URL of the REST API
-    todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        employee_id)
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        employee_id)
 
     try:
-        # Send GET request to the API
         todos_response = requests.get(todos_url)
         user_response = requests.get(user_url)
-        todos_response.raise_for_status()  # Raise exception for bad status codes
-        user_response.raise_for_status()  # Raise exception for bad status codes
+        todos_response.raise_for_status()
+        user_response.raise_for_status()
 
-        # Parse JSON response
         todos = todos_response.json()
         user = user_response.json()
 
-        # Extract employee name
         employee_name = user['name']
 
-        # Count completed tasks
         completed_tasks = [todo for todo in todos if todo['completed']]
         num_completed_tasks = len(completed_tasks)
         total_tasks = len(todos)
 
-        # Print employee TODO list progress
-        print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_completed_tasks, total_tasks))
         for todo in completed_tasks:
             print("\t {}".format(todo['title']))
 
     except requests.exceptions.HTTPError as errh:
-        print ("HTTP Error:",errh)
+        print("HTTP Error:", errh)
     except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:",errc) 
+        print("Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:",errt)
+        print("Timeout Error:", errt)
     except requests.exceptions.RequestException as err:
-        print ("Something Else:",err)
+        print("Something Else:", err)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
